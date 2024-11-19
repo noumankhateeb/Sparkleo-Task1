1b42b87d5d04c1a2d7b15a03068a0a1d9d5581ba
noumankhateeb <noumankhateebnk@gmail.com>
2024-11-20 03:29:22 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
